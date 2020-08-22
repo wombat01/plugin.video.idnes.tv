9d612bd9491e43376770063ed2003a7df85a9ecd
Gabriel <20426629+wombat01@users.noreply.github.com>
2020-08-22 13:38:37 +0200

    [plugin.video.idnes.tv] 1.0.5

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -34,8 +34,8 @@ def list_shows():
         thumb = normalize_url(re.search('url\(\'(.+)\'\)', porad.find('div', {'class': 'art-img'})['style']).group(1))
 
         list_item = xbmcgui.ListItem(label=title)
-        list_item.setInfo('video', {'mediatype': 'tvshow', 'tvshowtitle': title})
-        list_item.setArt({'icon': thumb})
+        list_item.setInfo('video', {'mediatype': 'tvshow', 'title': title})
+        list_item.setArt({'poster': thumb})
         listing.append((plugin.url_for(get_list, category_id = 'list', show_url = url), list_item, True))
 
     xbmcplugin.addDirectoryItems(plugin.handle, listing, len(listing))
@@ -53,15 +53,16 @@ def list_news():
         url = normalize_url(porad.find('a')['href'])
         list_item = xbmcgui.ListItem(label=title)
         list_item.setInfo('video', {'mediatype': 'tvshow', 'tvshowtitle': title})
-        listing.append((plugin.url_for(get_list, category_id = 'list', show_url = url), list_item, True))
+        listing.append((plugin.url_for(get_list, show_url = url, category = 0), list_item, True))
 
     xbmcplugin.addDirectoryItems(plugin.handle, listing, len(listing))
     xbmcplugin.endOfDirectory(plugin.handle)
 
-@plugin.route('/get_list/<category_id>')
-def get_list(category_id):
+@plugin.route('/get_list/')
+def get_list():
     xbmcplugin.setContent(plugin.handle, 'episodes')
     url = plugin.args['show_url'][0]
+    category = int(plugin.args['category'][0] if 'category' in plugin.args else 0)
     soup = BeautifulSoup(get_page(url), 'html.parser')
     items = soup.find('div', {'class': 'entry-list'}).find_all('div', {'class': 'entry'})
     listing = []
@@ -76,11 +77,11 @@ def get_list(category_id):
         show_title = soup.find('div', {'class': 'opener-in'})
         if show_title:
             show_title = show_title.find('h1').get_text().encode('utf-8')
-        if category_id == 'recent':
+        if category == 1:
             show_id = normalize_url(item.find('a', {'class': 'isle-link'})['href'])
             show_title = item.find('a', {'class': 'isle-link'}).get_text().encode('utf-8')
             title_label = '[COLOR blue]{0}[/COLOR] · {1}'.format(show_title, title)
-            menuitems.append(( _addon.getLocalizedString(30005), 'XBMC.Container.Update('+plugin.url_for(get_list, category_id = 'list', show_url = show_id)+')' ))
+            menuitems.append(( _addon.getLocalizedString(30005), 'XBMC.Container.Update('+plugin.url_for(get_list, show_url = show_id, category = 0)+')' ))
         if dur:
             l = dur.strip().split(':')
             duration = 0
@@ -100,7 +101,7 @@ def get_list(category_id):
     if next_url:
         list_item = xbmcgui.ListItem(label=_addon.getLocalizedString(30003))
         list_item.setArt({'icon': 'DefaultFolder.png'})
-        listing.append((plugin.url_for(get_list, category_id = category_id, show_url = next_url), list_item, True))
+        listing.append((plugin.url_for(get_list,  show_url = next_url, category = category), list_item, True))
 
     xbmcplugin.addDirectoryItems(plugin.handle, listing, len(listing))
     xbmcplugin.endOfDirectory(plugin.handle)
@@ -132,7 +133,7 @@ def root():
     listing = []
     list_item = xbmcgui.ListItem(_addon.getLocalizedString(30001))
     list_item.setArt({'icon': 'DefaultRecentlyAddedEpisodes.png'})
-    listing.append((plugin.url_for(get_list, category_id = 'recent', show_url = _baseurl+'archiv'), list_item, True))
+    listing.append((plugin.url_for(get_list, show_url = _baseurl+'archiv', category = 1), list_item, True))
 
     list_item = xbmcgui.ListItem(_addon.getLocalizedString(30002))
     list_item.setArt({'icon': 'DefaultTVShows.png'})
